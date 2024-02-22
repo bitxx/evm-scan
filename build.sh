5353d9324e245e86e09b0baa182613a41a0441c8
Jason <none>
2024-02-22 16:27:57 +0800

    问题修复

--- build.sh
+++ build.sh
@@ -4,26 +4,26 @@ go mod tidy
 
 # macos arm64
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o scan main.go
-tar czvf "evm-inscriptions_-macos-arm64".tar.gz scan settings.yml app.sql README.md
+tar czvf "evm-scan_-macos-arm64".tar.gz scan settings.yml app.sql README.md
 rm -f scan
 
 sleep 3
 
 # macos amd64
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o scan main.go
-tar czvf "evm-inscriptions-macos-amd64".tar.gz scan settings.yml README.md
+tar czvf "evm-scan-macos-amd64".tar.gz scan settings.yml README.md
 rm -f scan
 
 sleep 3
 
 # 交叉编译windows
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o scan.exe main.go
-tar czvf "evm-inscriptions-windows".tar.gz scan.exe settings.yml README.md
+tar czvf "evm-scan-windows".tar.gz scan.exe settings.yml README.md
 rm -f scan.exe
 
 sleep 3
 
 # 交叉编译linux
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o scan main.go
-tar czvf "evm-inscriptions-linux-amd64".tar.gz scan settings.yml README.md
+tar czvf "evm-scan-linux-amd64".tar.gz scan settings.yml README.md
 rm -f scan
